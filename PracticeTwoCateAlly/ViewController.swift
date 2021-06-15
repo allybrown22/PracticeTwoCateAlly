8b882587473041b4ddd3cb9c499396cb28931f9d
ally <aeb9226@gmail.com>
2021-06-15 14:31:35 -0400

    Initial Commit

---
+++ PracticeTwoCateAlly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeTwoCateAlly
+//
+//  Created by Ally Brown on 6/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
